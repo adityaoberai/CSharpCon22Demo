270a0655cdec9a4df6f878339ee46b9ef724d94d
adityaoberai <adityaoberai1@gmail.com>
2022-10-09 12:54:02 +0530

    Add capture image function

--- CSharpCon22Demo/MainPage.xaml.cs
+++ CSharpCon22Demo/MainPage.xaml.cs
@@ -14,11 +14,35 @@ public partial class MainPage : ContentPage
 		count++;
 
 		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
+			DetectEmotionButton.Text = $"Clicked {count} time";
 		else
-			CounterBtn.Text = $"Clicked {count} times";
+            DetectEmotionButton.Text = $"Clicked {count} times";
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		SemanticScreenReader.Announce(DetectEmotionButton.Text);
 	}
+
+	private async void CaptureImageButton_Clicked(object sender, EventArgs e)
+	{
+        if (MediaPicker.Default.IsCaptureSupported)
+        {
+            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+
+            if (photo != null)
+            {
+                string localFilePath = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                using Stream sourceStream = await photo.OpenReadAsync();
+                using FileStream localFileStream = File.OpenWrite(localFilePath);
+
+                await sourceStream.CopyToAsync(localFileStream);
+
+				CapturedImage.Source = ImageSource.FromFile(localFilePath);
+            }
+        }
+		else
+		{
+			await DisplayAlert("MediaPicker", "Not Supported", "Ok");
+		}
+    }
 }
 
