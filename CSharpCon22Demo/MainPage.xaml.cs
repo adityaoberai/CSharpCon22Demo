b7df24713f65ecaee03470b489b9e534c9fc5b52
adityaoberai <adityaoberai1@gmail.com>
2022-10-10 14:53:03 +0530

    Add extract text function

--- CSharpCon22Demo/MainPage.xaml.cs
+++ CSharpCon22Demo/MainPage.xaml.cs
@@ -1,31 +1,23 @@
-﻿namespace CSharpCon22Demo;
+﻿using CSharpCon22Demo.Helpers;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+
+namespace CSharpCon22Demo;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    public string filePath;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			DetectEmotionButton.Text = $"Clicked {count} time";
-		else
-            DetectEmotionButton.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(DetectEmotionButton.Text);
 	}
 
 	private async void CaptureImageButton_Clicked(object sender, EventArgs e)
 	{
         if (MediaPicker.Default.IsCaptureSupported)
         {
-            FileResult photo = await MediaPicker.Default.CapturePhotoAsync();
+            var photo = await MediaPicker.CapturePhotoAsync();
 
             if (photo != null)
             {
@@ -37,12 +29,52 @@ public partial class MainPage : ContentPage
                 await sourceStream.CopyToAsync(localFileStream);
 
 				CapturedImage.Source = ImageSource.FromFile(localFilePath);
+                filePath = localFilePath;
             }
         }
 		else
 		{
 			await DisplayAlert("MediaPicker", "Not Supported", "Ok");
 		}
+    }
+
+    private async void ExtractTextButton_Clicked(object sender, EventArgs e)
+    {
+        var credentials = new CognitiveServicesHelper();
+        var client = new ComputerVisionClient(new ApiKeyServiceClientCredentials(credentials.ComputerVisionAPIKey)) { Endpoint = credentials.ComputerVisionAPIEndpoint };
+
+        // Read text using local image path
+        using FileStream capture = File.OpenRead(filePath);
+        var textHeaders = await client.ReadInStreamAsync(capture);
+
+        // After the request, get the operation location (operation ID)
+        string operationLocation = textHeaders.OperationLocation;
+        Thread.Sleep(2000);
+
+        // Retrieve the URI where the extracted text will be stored from the Operation-Location header.
+        // We only need the ID and not the full URL
+        const int numberOfCharsInOperationId = 36;
+        string operationId = operationLocation.Substring(operationLocation.Length - numberOfCharsInOperationId);
+
+        // Extract the text
+        ReadOperationResult results;
+        do
+        {
+            results = await client.GetReadResultAsync(Guid.Parse(operationId));
+        }
+        while (results.Status == OperationStatusCodes.Running || results.Status == OperationStatusCodes.NotStarted);
+
+        // Display the found text.
+        var textUrlFileResults = results.AnalyzeResult.ReadResults;
+        string output = "";
+        foreach (ReadResult page in textUrlFileResults)
+        {
+            foreach (Line line in page.Lines)
+            {
+                output += line.Text + "\n";
+            }
+        }
+        await DisplayAlert("Output", output, "Ok");
     }
 }
 
